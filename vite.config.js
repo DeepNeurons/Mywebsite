25c5d62270c42a1af597197a75e5769f40276b63
DeepNeurons <bouchelligamedhedi@gmail.com>
2024-11-09 15:53:18 +0100

    refresh website

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Mywebsite/', // Replace with your repo name if deploying to GitHub Pages
   plugins: [react()],
   css: {
     modules: {
